08d26020d290163bf86533daba4da2613683bdf3
takahiko yoshizawa <taka0206@me.com>
2016-11-23 15:06:00 +0900

    [追加] chapter8

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - Hello World
+ * app.js - Express server static files
 */
 
 /*jslint         node    : true, continue : true,
@@ -10,12 +10,45 @@
 */
 /*global */
 
-var http, server;
+// ------------ BEGIN MODULE SCOPE VARIABLES --------------
+'use strict';
+var
+  http    = require( 'http'    ),
+  express = require( 'express' ),
 
-http   = require( 'http' );
-server = http.createServer( function ( request, response ) {
-  response.writeHead( 200, { 'Content-Type': 'text/plain' } );
-  response.end( 'Hello World' );
-}).listen( 3000 );
+  app     = express(),
+  server  = http.createServer( app );
+// ------------- END MODULE SCOPE VARIABLES ---------------
 
-console.log( 'Listening on port %d', server.address().port );
+// ------------- BEGIN SERVER CONFIGURATION ---------------
+app.configure( function () {
+  app.use( express.bodyParser() );
+  app.use( express.methodOverride() );
+  app.use( express.static( __dirname + '/public' ) );
+  app.use( app.router );
+});
+
+app.configure( 'development', function () {
+  app.use( express.logger() );
+  app.use( express.errorHandler({
+    dumpExceptions : true,
+    showStack      : true
+  }) );
+});
+
+app.configure( 'production', function () {
+  app.use( express.errorHandler() );
+});
+
+app.get( '/', function ( request, response ) {
+  response.redirect( '/spa.html' );
+});
+// -------------- END SERVER CONFIGURATION ----------------
+
+// ----------------- BEGIN START SERVER -------------------
+server.listen( 3000 );
+console.log(
+  'Express server listening on port %d in %s mode',
+   server.address().port, app.settings.env
+);
+// ------------------ END START SERVER --------------------
